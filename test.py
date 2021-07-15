e649bdbf9c0c84709378575a3163b2df87433abb
Chang Xiao <xiaochangfeng@gmail.com>
2021-07-15 13:04:03 -0400

    adding test set

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-from tests.test_reduce import test_reduce
+from tests.test_train import test_train
+from tests.test_train import test_test
 
-test = test_reduce()
+print('Training the data set....')
+train = test_train()
+train.run()
+
+print('\n\nDecomposing a test set...')
+test = test_test()
 test.run()
--- tests/test_train.py
+++ tests/test_train.py
@@ -2,18 +2,22 @@ import os, pathlib
 import numpy as np
 from reduce import pca_experiment
 
-class test_reduce():
+'''
+Testing the PCA algorithm on a training set
+'''
+class test_train():
 
     def __init__(self):
         config_dir = os.path.dirname(os.path.realpath(__file__))
         config_dir_path = pathlib.Path(config_dir)
         app_dir = config_dir_path.parent
         self.app_dir = app_dir
-        self.ids, features = self.load('test.txt')
+        self.ids, features = self.load('train.txt')
         self.variance_retained = .99
-        self.pca = pca_experiment(features, 'test.txt', self.variance_retained, f'{self.app_dir}/output')
+        self.pca = pca_experiment(features, 'train.txt', self.variance_retained, f'{self.app_dir}/output')
 
     def run(self):
+        print(f"Training set shape: {self.pca.features.shape[0]},{self.pca.features.shape[1]}")
         print(f"Number of components before PCA: {self.pca.features.shape[1]}")
         self.pca.dump()
         self.dump_labels()
@@ -23,7 +27,7 @@ class test_reduce():
 
 
     def dump_labels(self):
-        with open(f"{self.app_dir}/output/test_label.txt", "w") as filehandle:
+        with open(f"{self.app_dir}/output/train_label.txt", "w") as filehandle:
             ids = list(map(lambda x: x + '\n', self.ids))
             filehandle.writelines(ids)
 
@@ -35,7 +39,7 @@ class test_reduce():
         features = []
         unique_ids = []
         try:
-            fh = open(f"{self.app_dir}/tests/test.txt", "r")
+            fh = open(f"{self.app_dir}/tests/{file_name}", "r")
             for line in fh:      
                 id, _feature = str(line).split('|')
                 feature = _feature.split(',')
@@ -52,3 +56,24 @@ class test_reduce():
 
         except FileNotFoundError:
             return [False, False]
+
+'''
+Testing the PCA algorithm on a test set data (1 sample)
+'''
+class test_test(test_train):
+    def __init__(self):
+        config_dir = os.path.dirname(os.path.realpath(__file__))
+        config_dir_path = pathlib.Path(config_dir)
+        self.app_dir = config_dir_path.parent
+        self.ids, features = self.load('test.txt')
+        self.variance_retained = .99
+        # We are passing in the train.txt to look for the fitted metrics from the training set
+        self.pca = pca_experiment(features, 'train.txt', self.variance_retained, f'{self.app_dir}/output')
+
+    def run(self):
+        print(f"Performing PCA on a test set of shape: {self.pca.features.shape[0]},{self.pca.features.shape[1]}")
+        reduced_features = self.pca.decompose('transform')
+        print('Shape after performing PCA')
+        print(reduced_features.shape)
+        print('Dump')
+        print(reduced_features)
